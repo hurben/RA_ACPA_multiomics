e13e09f14585dde8c5a35b70e4200427f13da0ef
hurben <hurben@bioinfo@gmail.com>
2023-04-28 12:35:21 -0500

    update

---
+++ analysis/correlation_network/topology_data/02_get_sig_network.sh
@@ -0,0 +1,3 @@
+nohup python3 ../../../src/correlation_network/STEP4_topology_with_threshold.py acpa_neg_3_omics &
+nohup python3 ../../../src/correlation_network/STEP4_topology_with_threshold.py acpa_pos_3_omics &
+nohup python3 ../../../src/correlation_network/STEP4_topology_with_threshold.py control_3_omics &
