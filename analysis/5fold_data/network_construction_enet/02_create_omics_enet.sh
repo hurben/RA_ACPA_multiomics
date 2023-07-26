3310e33c0de756eb2a655efb1499d1d26d835dbb
hurben <hurben@bioinfo@gmail.com>
2023-07-26 12:14:39 -0500

    update

--- analysis/5fold_data/network_construction_enet/02_create_omics_enet.sh
+++
@@ -1,3 +0,0 @@
-#disabled in here (meant to be runned in server)
-#python3 ../../../src/network_construction_10fold/enet_construction.short.py
-#python3 ../../../src/network_construction_10fold/enet_construction.py
